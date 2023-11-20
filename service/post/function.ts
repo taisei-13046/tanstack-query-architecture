498e0b4a30eb7a8f0fc5151e69f617e5ad817b9d
taisei-13046 <78260526+taisei-13046@users.noreply.github.com>
2023-11-20 22:27:54 +0000

    Update function.ts

--- service/post/function.ts
+++ service/post/function.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { GetPostsResponse } from './type';
 
-export const getPosts = async (): Promise<GetPostsResponse[]> => {
+export const getPosts = async (): Promise<GetPostsResponse> => {
   const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
   return res.data;
 };
